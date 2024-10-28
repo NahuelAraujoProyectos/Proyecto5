7651f80b833974f9c04bbfefaf1ad2ca10a2d260
Nahuel Araujo <nahuelaraujo1991@gmail.com>
2024-10-28 19:49:11 +0100

    Primer intento

---
+++ product-template/src/main/java/com/name/productstorage/controller/ProductController.java
@@ -0,0 +1,67 @@
+package com.name.productstorage.controller;
+
+import com.name.productstorage.model.Product;
+import com.name.productstorage.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        try {
+            Product savedProduct = productService.saveProduct(product);
+            return new ResponseEntity<>(savedProduct, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping
+    public CompletableFuture<ResponseEntity<List<Product>>> getAllProducts() {
+        try {
+            CompletableFuture<List<Product>> products = productService.getAllProducts();
+            return products.thenApply(product -> ResponseEntity.ok().body(product));
+        } catch (Exception e){
+            return CompletableFuture.completedFuture(ResponseEntity.notFound().build());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable("id") Integer id) {
+        Optional<Product> productData = productService.getProductById(id);
+        return productData.map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") Integer id, @RequestBody Product product) {
+        try {
+            Product updatedProduct = productService.updateProduct(id, product);
+            return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteProduct(@PathVariable("id") Integer id) {
+        try {
+            productService.deleteProduct(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
